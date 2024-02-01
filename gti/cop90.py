64b911d4e6f3581ebe7c61855bb5a6f6e8dc5db2
Michael Sumner <mdsumner@gmail.com>
2024-02-01 10:50:07 +0000

    update the cop90 vector layer

--- gti/cop90.py
+++ gti/cop90.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 
 from osgeo import gdal, ogr, osr
 
-
+## create a polygon in gdal from xmin,xmax,ymin,ymax
 def create_poly(extent):
     # Create a Polygon from the extent list
     ring = ogr.Geometry(ogr.wkbLinearRing)
@@ -18,21 +18,27 @@ def create_poly(extent):
 
     return poly
 
-
 gdal.UseExceptions()
 
+## an OpenTopography VRT represents a huge, sparse, global DEM raster  mosaic (land regions)
 urldirname = "https://opentopography.s3.sdsc.edu/raster/COP90"
 basename = "COP90_hh.vrt"
 url = f"{urldirname}/{basename}"
+
+## we will write an index of each source GeoTIFF to a FlatGeobuf polygon layer with 'location'
+## of each GeoTIFF url
 layer_name = "cop90"
 drivername = "FlatGeobuf"
 fgb_path = f"{layer_name}.gti.fgb"
 
+## open the data set and get extent and resolution (we'll populate the GTI vector layer with this md)
 vrt = gdal.Open(f"/vsicurl/{url}")
 gt = vrt.GetGeoTransform()
 resxy = [gt[1], -gt[5]]
 exxy = [gt[0], gt[0] + vrt.RasterXSize * resxy[0],
         gt[3] + vrt.RasterYSize * -resxy[1], gt[3]]
+        
+## read and parse the VRT xml from url, get extents and source url 
 con = urlopen(url)
 tree = ET.parse(con)
 
@@ -60,10 +66,11 @@ for item in tree.findall(".//SimpleSource"):
 
     file_extents[srcfilename.text] = extent
 
+## map projection string for the vector layer (don't use OGC:CRS84 because the driver doesn't materialize this in full)
 sr = osr.SpatialReference()
 sr.SetFromUserInput("EPSG:4326")
 
-
+## create the vector layer and set the metadata of the virtual raster (so we avoid opening any source files for info)
 ds = ogr.GetDriverByName(drivername).CreateDataSource(fgb_path)
 layer = ds.CreateLayer(layer_name, geom_type=ogr.wkbPolygon, srs=sr)
 layer.SetMetadataItem("RESX", str(resxy[0]))
@@ -77,7 +84,7 @@ layer.SetMetadataItem("MAXY", str(exxy[3]))
 layer.SetMetadataItem("BAND_COUNT", "1")
 layer.SetMetadataItem("SRS", "EPSG:4326")
 
-# Add an ID field
+# Add an ID field, and 'location' field (the url of the source GeoTIFF)
 idField = ogr.FieldDefn("id", ogr.OFTInteger)
 layer.CreateField(idField)
 locField = ogr.FieldDefn("location", ogr.OFTString)
